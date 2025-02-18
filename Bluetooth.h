3b061546d7a1ba37ba7f4a8b7ac5f2b0254d3756
Paul Schulze <avlex82@gmail.com>
2025-02-18 22:47:32 +0100

    Remove forward declaration of private API HapticWrite

--- Bluetooth.h
+++ Bluetooth.h
@@ -10,63 +10,61 @@
 namespace TinyCon
 {
 #if NO_BLE
-class BluetoothController
-{
-public:
-    BluetoothController(GamepadController& controller, CommandProcessor& processor) {}
-    void Init() {}
-    void Update(uint32_t) {}
-    void SetActive(bool) {}
-    [[nodiscard]] bool IsConnected() const { return false; }
-    [[nodiscard]] bool IsAdvertising() const { return false; }
-};
-#else
-#include <bluefruit.h>
-#include <services/BLEHIDGamepad.h>
-
-void HapticWrite(uint16_t connection, BLECharacteristic* chr, uint8_t* data, uint16_t length);
-
-class BluetoothController
-{
-public:
-    BluetoothController(const GamepadController& controller, CommandProcessor& processor)
-        : Controller(controller), Processor(processor) {}
-
-    void Init();
-    void Update(uint32_t deltaTime);
-    void SetActive(bool active)
+    class BluetoothController
     {
-        Serial.print(active ? "Bluetooth set Active" : "Bluetooth set Inactive"); Serial.println(Active ? " from Active" : " from Inactive");
-        if (active && !Active) ForceAdvertise = true;
-        Active = active;
-    }
+    public:
+        BluetoothController(GamepadController& controller, CommandProcessor& processor) {}
+        void Init() {}
+        void Update(uint32_t) {}
+        void SetActive(bool) {}
+        [[nodiscard]] bool IsConnected() const { return false; }
+        [[nodiscard]] bool IsAdvertising() const { return false; }
+    };
+#else
+    #include <bluefruit.h>
+    #include <services/BLEHIDGamepad.h>
 
-    [[nodiscard]] bool IsActive() const { return Active || Connected; }
-    [[nodiscard]] bool IsAdvertising() const { return ForceAdvertise || AdvertisingTimeout > 0; }
-    [[nodiscard]] bool IsConnected() const { return Connected; }
+    class BluetoothController
+    {
+    public:
+        BluetoothController(const GamepadController& controller, CommandProcessor& processor)
+            : Controller(controller), Processor(processor) {}
 
-private:
-    static std::function<void(uint16_t, BLECharacteristic*, uint8_t*, uint16_t)> HapticWriteCallback;
-    static void HapticWrite(uint16_t connection, BLECharacteristic* chr, uint8_t* data, uint16_t length);
+        void Init();
+        void Update(uint32_t deltaTime);
+        void SetActive(bool active)
+        {
+            Serial.print(active ? "Bluetooth set Active" : "Bluetooth set Inactive"); Serial.println(Active ? " from Active" : " from Inactive");
+            if (active && !Active) ForceAdvertise = true;
+            Active = active;
+        }
 
-    bool Active = false;
-    bool Connected = false;
+        [[nodiscard]] bool IsActive() const { return Active || Connected; }
+        [[nodiscard]] bool IsAdvertising() const { return ForceAdvertise || AdvertisingTimeout > 0; }
+        [[nodiscard]] bool IsConnected() const { return Connected; }
 
-    BLEDis Discovery;
-    BLEHidGamepad GamepadService;
-    BLEService MpuService = BLEService(0x181A);
-    BLECharacteristic MpuCharacteristic = BLECharacteristic(0x2A58);
-    BLEService HapticService = BLEService(0x1812);
-    BLECharacteristic HapticCharacteristic = BLECharacteristic(0x2A4D);
-    uint16_t ConnectionId = 0;
+    private:
+        static std::function<void(uint16_t, BLECharacteristic*, uint8_t*, uint16_t)> HapticWriteCallback;
+        static void HapticWrite(uint16_t connection, BLECharacteristic* chr, uint8_t* data, uint16_t length);
 
-    constexpr static uint32_t AdvertisingTime = 40000;
-    int32_t AdvertisingTimeout = 0;
-    bool ForceAdvertise = false;
-    bool AdvertisingStarted = false;
+        bool Active = false;
+        bool Connected = false;
 
-    const GamepadController& Controller;
-    CommandProcessor& Processor;
-};
+        BLEDis Discovery;
+        BLEHidGamepad GamepadService;
+        BLEService MpuService = BLEService(0x181A);
+        BLECharacteristic MpuCharacteristic = BLECharacteristic(0x2A58);
+        BLEService HapticService = BLEService(0x1812);
+        BLECharacteristic HapticCharacteristic = BLECharacteristic(0x2A4D);
+        uint16_t ConnectionId = 0;
+
+        constexpr static uint32_t AdvertisingTime = 40000;
+        int32_t AdvertisingTimeout = 0;
+        bool ForceAdvertise = false;
+        bool AdvertisingStarted = false;
+
+        const GamepadController& Controller;
+        CommandProcessor& Processor;
+    };
 #endif
 }
